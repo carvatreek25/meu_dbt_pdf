7d94eb4f8e0fe6f9c4639a07bfaefd99fd494433
Amanda Ribeiro <amanda_carvalheira_1@hotmail.com>
2025-12-22 13:05:40 -0300

    Add PDF and CSV example files

---
+++ files/pdf/pdf_viz.py
@@ -0,0 +1,37 @@
+import os
+import camelot
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+from unidecode import unidecode
+
+# Seu comando do Ghostscript
+os.environ['PATH'] += r';C:\Program Files\gs\gs10.06.0\bin'
+
+# Seu arquivo (como o professor fez)
+file_name = 'Redrex - Fatura (1)'
+path = os.path.abspath(f"meu_dbt_pdf/files/pdf/redrex/{file_name}.pdf")
+
+# Usar a variável 'path' no camelot
+tables = camelot.read_pdf(
+    path,
+    pages='1-end',
+    flavor='stream',
+    table_areas=['65 , 558 , 500 , 298'],
+    columns=["65,107,156,212,336,383,450"],
+    strip_text='\n'
+    )
+
+# Exibe o relatório de acurácia (Parsing Report)
+print(tables[0].parsing_report)
+print(tables[0].df)
+
+#camelot.plot(tables[0], kind='contour')
+
+#plt.show()
+
+print("Pause")
+
+
+#python meu_dbt_pdf/files/pdf_viz.py
---
+++ files/pdf/rules/regras.py
@@ -0,0 +1,25 @@
+jornada = {
+        "name": "Jornada",
+        "table_areas": [""],
+        "columns": [""],
+        "flavor": "stream",
+        "pages": "1-end",
+        "strip_text": " ./n",
+        "password": None    
+}
+
+redrex = {
+    "name': "Redrex",
+    "table_areas": [""],
+    "columns": [""],
+    "flavor": "stream",
+    "pages": "1-end",
+    "strip_text": " ./n",
+    "password": None    
+    
+}
+
+rules_dict = {
+    "jornada": jornada,
+    "redrex": redrex
+}
